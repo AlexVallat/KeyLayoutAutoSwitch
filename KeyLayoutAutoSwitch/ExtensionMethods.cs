2b36af93ea652718f528b9e15b556a6835136ba4
Alex Vallat <git@alex.vallat.name>
2020-12-11 19:53:18 +0000

    Improved lookup of keyboard layout names (issue #3)

--- KeyLayoutAutoSwitch/ExtensionMethods.cs
+++ KeyLayoutAutoSwitch/ExtensionMethods.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Windows.Forms;
 
@@ -7,43 +9,65 @@ namespace KeyLayoutAutoSwitch
 	internal static class ExtensionMethods
 	{
 		private static readonly MethodInfo GetLocalizedKeyboardLayoutNameMethod = typeof(InputLanguage).GetMethod("GetLocalizedKeyboardLayoutName", BindingFlags.NonPublic | BindingFlags.Static);
-		public static string GetLayoutName(this InputLanguage inputLanguage)
-		{
-			var handle = (uint)inputLanguage.Handle;
-			if ((handle & 0xf0000000) == 0xf0000000) // InputLanguage.LayoutName is bugged when layout ID is provided, so try and do better
-			{
-				var targetLanguage = (handle & 0xffff).ToString("X4");
-				var targetLayoutId = ((handle >> 16) & 0x0fff).ToString("X4");
 
-				var keyboardLayouts = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Keyboard Layouts");
+		private static readonly Dictionary<uint, string> _layoutKeyNamesByLayoutId = LoadLayoutKeyNames();
+		private static readonly Dictionary<IntPtr, string> _layoutNameCache = new Dictionary<IntPtr, string>();
+
+		private static Dictionary<uint, string> LoadLayoutKeyNames()
+		{
+			var layoutKeyNames = new Dictionary<uint, string>();
+
+			using (var keyboardLayouts = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Keyboard Layouts"))
+			{
 				foreach (var keyboardLayout in keyboardLayouts.GetSubKeyNames())
 				{
-					if (keyboardLayout.Length == 8)
+					using (var keyboardLayoutKey = keyboardLayouts.OpenSubKey(keyboardLayout))
 					{
-						var layoutLanguage = keyboardLayout.Substring(4);
-						if (layoutLanguage == targetLanguage)
+						var layoutId = keyboardLayoutKey.GetValue("Layout Id") as string;
+						if (layoutId != null)
 						{
-							// Does this match the layout too?
-							using (var keyboardLayoutKey = keyboardLayouts.OpenSubKey(keyboardLayout))
-							{
-								var layoutId = keyboardLayoutKey.GetValue("Layout Id") as string;
-								if (layoutId == targetLayoutId)
-								{
-									// Match by language and layout ID,
-
-
-									// Attempt to extract the localized keyboard layout name, default back to legacy name from registry, final default just use InputLanguage code path
-									return GetLocalizedKeyboardLayoutName(keyboardLayoutKey.GetValue("Layout Display Name") as string)
-										?? keyboardLayoutKey.GetValue("Layout Text") as string
-										?? inputLanguage.LayoutName;
-								}
-							}
+							layoutKeyNames[Convert.ToUInt32(layoutId, 16)] = keyboardLayout;
 						}
 					}
 				}
 			}
 
-			return inputLanguage.LayoutName;
+			return layoutKeyNames;
+		}
+
+		public static string GetLayoutName(this InputLanguage inputLanguage)
+		{
+			if (!_layoutNameCache.TryGetValue(inputLanguage.Handle, out var layoutName))
+			{
+				// InputLanguage.LayoutName is bugged (https://github.com/dotnet/winforms/issues/4345), try to do better
+				try
+				{
+					var targetLayoutIdOrLanguage = (uint)inputLanguage.Handle >> 16;
+
+					var keyName = (targetLayoutIdOrLanguage & 0xf000) == 0xf000 ?
+							// This is a layout ID, so get the key name from the lookup
+							_layoutKeyNamesByLayoutId[targetLayoutIdOrLanguage & 0x0fff]
+						:
+							// This is a language ID, the layout name is just that.
+							targetLayoutIdOrLanguage.ToString("X8");
+
+					using (var keyboardLayoutKey = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Keyboard Layouts\" + keyName))
+					{
+						// Attempt to extract the localized keyboard layout name, default back to legacy name from registry, final default just use InputLanguage code path
+						layoutName = GetLocalizedKeyboardLayoutName(keyboardLayoutKey.GetValue("Layout Display Name") as string)
+										?? keyboardLayoutKey.GetValue("Layout Text") as string
+										?? inputLanguage.LayoutName;
+					}
+				}
+				catch
+				{
+					// Fallback on the buggy WinForms implemenation
+					layoutName = inputLanguage.LayoutName;
+				}
+				_layoutNameCache.Add(inputLanguage.Handle, layoutName);
+			}
+
+			return layoutName;
 		}
 
 		private static string GetLocalizedKeyboardLayoutName(string layoutDisplayName)
